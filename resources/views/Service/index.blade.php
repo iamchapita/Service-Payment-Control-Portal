12a62e46a76ddc47f33071c037ee93613e0fb944
iamchapita <alejandrom646@gmail.com>
2022-10-22 16:17:25 -0600

    Implementacion de vista para Tabla Service con respectivo CSS

--- resources/views/Service/index.blade.php
+++ resources/views/Service/index.blade.php
@@ -1,17 +1,20 @@
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Nombre</th>
-        </tr>
-    </thead>
+@include('navbar')
+<div class='container'>
+    <table class="table table-bordered table-ligth table-striped">
+        <thead  class="table-dark">
+            <tr>
+                <th>#</th>
+                <th>Nombre</th>
+            </tr>
+        </thead>
 
-    <tbody>
-        @foreach ( $values as $value )
-        <tr>
-            <td>{{ $value->id }}</td>
-            <td>{{ $value->texName }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+        <tbody>
+            @foreach ( $values as $value )
+            <tr>
+                <td>{{ $value->id }}</td>
+                <td>{{ $value->texName }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>

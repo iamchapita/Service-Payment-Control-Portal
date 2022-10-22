902fb03ce1ed61aec4c4d9219a6b68ecdb5eb882
iamchapita <alejandrom646@gmail.com>
2022-10-22 14:17:35 -0600

    Añadido para inclusión de etiquetas bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+</head>
+

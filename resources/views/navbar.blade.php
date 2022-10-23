4e473c0950456405cf9b7e89fe015b81f41f43e8
iamchapita <alejandrom646@gmail.com>
2022-10-23 03:20:51 -0600

    Implementacion de navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -17,34 +17,43 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{ $view }}</a>
+        <a class="navbar-brand" href="#">{{ $current }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+
+                @foreach ( $views as $view )
+                @if ( $view == $view )
+                <li class="nav-view">
+                    <a class="nav-link active" aria-current="page" href="#">{{ $view }}</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                @else
+                <li class="nav-view">
+                    <a class="nav-link" href="{{ url( $view ) }}">{{ $view }}</a>
                 </li>
-                <li class="nav-item dropdown">
+                @endif
+                @endforeach
+
+                @isset( $elementsDropdown )
+                <li class="nav-view dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Detalle de PaymentDetail
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        @foreach ( $elementsDropdown as $element )
+                        <li><a class="dropdown-item" href="#">{{ $element }}</a></li>
+                        @endforeach
                     </ul>
                 </li>
-                <li class="nav-item">
+                @endisset
+
+                <li class="nav-view">
                     <a class="nav-link disabled">Disabled</a>
                 </li>
+
+
             </ul>
         </div>
     </div>

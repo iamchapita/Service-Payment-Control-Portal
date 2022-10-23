2db6e12f8910bd3d399e4980b02905e1550fc9be
iamchapita <alejandrom646@gmail.com>
2022-10-23 03:18:58 -0600

    Implementacion de rutas para PaymentDetail

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PaymentDetailController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\UserController;
 use App\Models\Service;
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('Service', ServiceController::class);
 Route::resource('User', UserController::class);
+Route::resource('PaymentDetail', PaymentDetailController::class);

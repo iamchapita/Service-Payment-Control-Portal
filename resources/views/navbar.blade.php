a58a3a27588714caa9661d9070a78688db94ae67
iamchapita <alejandrom646@gmail.com>
2022-10-23 22:03:04 -0600

    Renombre de variables

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -17,7 +17,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{ $current }}</a>
+        <a class="navbar-brand" href="#">{{ $currentView }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -25,9 +25,9 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                 @foreach ( $views as $view )
-                @if ( $view == $current )
+                @if ( $view == $currentView )
                 <li class="nav-view">
-                    <a class="nav-link active" aria-current="page" href="#">{{ $view }}</a>
+                    <a class="nav-link active" aria-currentView="page" href="#">{{ $view }}</a>
                 </li>
                 @else
                 <li class="nav-view">

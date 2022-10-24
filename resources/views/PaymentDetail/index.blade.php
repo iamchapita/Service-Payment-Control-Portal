b255eb54efba529ba4dd3322be356d943a772666
iamchapita <alejandrom646@gmail.com>
2022-10-23 22:07:53 -0600

    Cambio de titulo estatico a titul por variable

--- resources/views/PaymentDetail/index.blade.php
+++ resources/views/PaymentDetail/index.blade.php
@@ -1,6 +1,6 @@
 @include('navbar')
 <div class='container'>
-    <h1>Histórico de Pago General</h1>
+    <h1>{{ $currentView}}</h1>
     <table class="table table-bordered table-ligth table-striped">
         <thead class="table-dark">
             <tr>

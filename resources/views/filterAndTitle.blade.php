a741248380b263ae71a0014b51361932ae105757
iamchapita <alejandrom646@gmail.com>
2022-10-28 16:01:30 -0600

    Creacion de plantilla para titulo y buscador

---
+++ resources/views/filterAndTitle.blade.php
@@ -0,0 +1,7 @@
+<div class="filterAndTitle">
+    <h1>{{ $currentView }}</h1>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" id="searchInput" onkeyup="searchFilter()" placeholder="Buscar" aria-label="Searc" aria-describedby="basic-addon1">
+        <span class="input-group-text" id="basic-addon1">@</span>
+    </div>
+</div>

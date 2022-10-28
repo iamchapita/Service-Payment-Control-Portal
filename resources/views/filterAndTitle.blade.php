30484abac5a32809fc963d21ef0529880683dd91
iamchapita <alejandrom646@gmail.com>
2022-10-28 16:42:09 -0600

    Agregado parametro para limpieza automatica del campo

--- resources/views/filterAndTitle.blade.php
+++ resources/views/filterAndTitle.blade.php
@@ -1,7 +1,6 @@
 <div class="filterAndTitle">
     <h1>{{ $currentView }}</h1>
     <div class="input-group mb-3">
-        <input type="text" class="form-control" id="searchInput" onkeyup="searchFilter()" placeholder="Buscar" aria-label="Searc" aria-describedby="basic-addon1">
-        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" id="searchInput" autocomplete="off" onkeyup="searchFilter()" placeholder="Buscar" aria-label="Searc" aria-describedby="basic-addon1">
     </div>
 </div>

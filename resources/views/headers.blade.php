f8836481e50f337f1644dd1a3773e6e659adad81
iamchapita <alejandrom646@gmail.com>
2022-10-28 23:11:11 -0600

    Se extrajo los headers de la plantilla narvbar

---
+++ resources/views/headers.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <script src="{{asset('js/filter.js')}}"></script>
+
+</head>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,21 +1,4 @@
-<html lang="es">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <script src="{{asset('js/filter.js')}}"></script>
-
-</head>
-
+@include('headers')
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">{{ $currentView }}</a>

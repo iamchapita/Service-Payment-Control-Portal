6e456f4eb85306928fefb878393cf50a93f6b189
iamchapita <alejandrom646@gmail.com>
2022-10-29 15:33:27 -0600

    Creacion de funcion para limpiar el campo select al pulsar los botones de accion del modal

---
+++ public/js/clearSelect.js
@@ -0,0 +1,11 @@
+// -*- coding: utf-8 -*-
+/*
+@author: lamorales@unah.hn || alejandrom646@gmail.com ||iamchapita
+@date: 2022/10/29
+@version: 0.1.0
+*/
+
+function clearSelect() {
+    var select = document.getElementsByTagName("select");
+    select[0].value = "Seleccione su Nombre";
+}

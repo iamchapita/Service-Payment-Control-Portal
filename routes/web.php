8c101f37f9fbd45c470441da2ff1a1334ddc9c9c
iamchapita <alejandrom646@gmail.com>
2022-10-28 23:10:44 -0600

    Agregada ruta de inicio

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('Service', ServiceController::class);
 Route::resource('User', UserController::class);
-// Route::resource('PaymentDetail', PaymentDetailController::class);
+Route::get('/', function() {
+    return view('FirstScreen.index');
+});
 
 // Rutas Personalizadas
 Route::prefix('/PaymentDetail')->group( function () {

b4b7ccc85f3617fae90eb6150ab57e7bd6421942
iamchapita <alejandrom646@gmail.com>
2022-10-29 18:20:10 -0600

    Se incluyo comprobacion de una variable

---
+++ resources/views/PaymentDetail/userDetailTable.blade.php
@@ -0,0 +1,29 @@
+@include('navbar')
+<div class='container'>
+    @include('filterAndTitle')
+    <div class="table-responsive">
+        <table class="table table-bordered table-ligth table-hover" id="table">
+            <thead class="table-dark">
+                <tr>
+                    <th>Usuario</th>
+                    <th>Servicio</th>
+                    <th>Mes</th>
+                    <th>Fecha Pago</th>
+                    <th>Cuota</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ( $values as $value )
+                <tr>
+                    <td>{{ $value->Usuario }}</td>
+                    <td>{{ $value->Servicio }}</td>
+                    <td>{{ $value->Mes }}</td>
+                    <td>{{ $value->Fecha }}</td>
+                    <td>{{ $value->Pago }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</div>

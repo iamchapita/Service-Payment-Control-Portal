2adf087c0f65dd6f613ccceda976c2b53523b676
iamchapita <alejandrom646@gmail.com>
2022-10-31 05:58:21 -0600

    Renombramiento de archivo

---
+++ resources/views/filterAndButton.blade.php
@@ -0,0 +1,8 @@
+<h1>{{ $currentView }}</h1>
+<br>
+<div id="filterAndButton">
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" id="searchInput" autocomplete="off" onkeyup="searchFilter()" placeholder="Filtrar" aria-label="Search" aria-describedby="basic-addon1">
+    </div>
+    <button type="button" class="btn btn-secondary" onclick="location.href=''">Insertar</button>
+</div>
--- resources/views/filterAndTitle.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="filterAndTitle">
-    <h1>{{ $currentView }}</h1>
-    <div class="input-group mb-3">
-        <input type="text" class="form-control" id="searchInput" autocomplete="off" onkeyup="searchFilter()" placeholder="Buscar" aria-label="Searc" aria-describedby="basic-addon1">
-    </div>
-</div>

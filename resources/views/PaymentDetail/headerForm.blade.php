85fb47c8121a4b5c8e88fd5758bdb370f429cce2
iamchapita <alejandrom646@gmail.com>
2022-11-04 23:47:27 -0600

    Muestra errores si existen

--- resources/views/PaymentDetail/headerForm.blade.php
+++ resources/views/PaymentDetail/headerForm.blade.php
@@ -1,10 +1,21 @@
 @include('navbar')
+
+@if(count($errors) > 0)
+    <div class="alert alert-danger" role="alert">
+        <ul>
+            @foreach($errors -> all() as $error)
+            <li>
+                {{ $error }}
+            </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <div class="container">
     @isset($title)
-    <h1>{{ $title }}</h1>
-    <br>
+        <h1>{{ $title }}</h1>
+        <br>
     @endisset
-
     @if(isset($values))
     @foreach ($values as $value)
     <form method="POST" action="{{ route($formURL, $value->id) }}" enctype="multipart/form-data">

3edd0d436d113f7473ddea2c3e5892505d201793
iamchapita <alejandrom646@gmail.com>
2022-11-05 01:28:14 -0600

    Formateo del documento

--- resources/views/PaymentDetail/paymentDetailForm.blade.php
+++ resources/views/PaymentDetail/paymentDetailForm.blade.php
@@ -1,20 +1,18 @@
 <div class="input-group mb-3">
-    <label class="input-group-text" for="userInput">Usuarios</label>
+    <label class="input-group-text" for="userInput">Usuario</label>
     <select name="userInput" class="form-select" id="userInput" required autocomplete="off">
         <option selected value="">Seleccione un Usuario</option>
         @foreach( $users as $user )
         @if(isset($values))
-        <option
-            @foreach($values as $value)
-                @if( $value->idUserFK == $user->id)
-                    selected
-                @endif
+        <option @foreach($values as $value) @if( $value->idUserFK == $user->id)
+            selected
+            @endif
             @endforeach
             value="{{ $user->id }}">
             {{ $user->texName }}
         </option>
         @else
-            <option value="{{ $user->id }}">{{ $user->texName }}</option>
+        <option value="{{ $user->id }}">{{ $user->texName }}</option>
         @endif
         @endforeach
     </select>
@@ -26,11 +24,9 @@
         <option selected value="">Seleccione un Servicio</option>
         @foreach( $services as $service )
         @if(isset($values))
-        <option
-            @foreach($values as $value)
-                @if( $value->idServiceFK == $service->id)
-                    selected
-                @endif
+        <option @foreach($values as $value) @if( $value->idServiceFK == $service->id)
+            selected
+            @endif
             @endforeach
             value="{{ $service->id }}">
             {{ $service->texName }}
@@ -48,11 +44,9 @@
         <option selected value="">Seleccione un Mes</option>
         @foreach( $months as $month )
         @if(isset($values))
-        <option
-            @foreach($values as $value)
-                @if( $value->idMonthFK == $month->id)
-                    selected
-                @endif
+        <option @foreach($values as $value) @if( $value->idMonthFK == $month->id)
+            selected
+            @endif
             @endforeach
             value="{{ $month->id }}">
             {{ $month->texName }}
@@ -66,80 +60,40 @@
 
 <div class="input-group mb-3">
     <label class="input-group-text" for="payDateInput">Fecha de Pago</label>
-    <input
-        type="date"
-        class="date form-control"
-        name="payDateInput"
-        id="payDateInput"
-        @if(isset($values))
-            @foreach($values as $value)
-                value={{ $value->datDate }}
-            @endforeach
-        @endif
-        required autocomplete="off"
-    >
+    <input type="date" class="date form-control" name="payDateInput" id="payDateInput" @if(isset($values)) @foreach($values as $value) value={{ $value->datDate }} @endforeach @endif required autocomplete="off">
 </div>
 
 <div class="input-group mb-3">
     <label class="input-group-text" for="moneyAmountInput">Cuota</label>
-    <input
-        type="number"
-        class="form-control"
-        name="moneyAmountInput"
-        id="moneyAmountInput"
-        @if(isset($values))
-            @foreach($values as $value)
-                value={{ $value->numPaid }}
-            @endforeach
-        @endif
-        required autocomplete="off"
-    >
+    <input type="number" class="form-control" name="moneyAmountInput" id="moneyAmountInput" @if(isset($values)) @foreach($values as $value) value={{ $value->numPaid }} @endforeach @endif required autocomplete="off">
 </div>
 
 <div class="input-group mb-3">
     <label class="input-group-text" for="depositStatus">Estado del Depósito</label>
     <select class="form-select" name="depositStatus" id="depositStatus" required autocomplete="off" onchange="checkDepositeStatus()">
         <option value="">Seleccione el Estado</option>
-        <option
-                value="0"
-                @if(isset($values))
-                    @foreach($values as $value)
-                        @if($value->boolDeposited == 0)
-                        selected
-                        @endif
-                    @endforeach
-                @endif
-        >
-        Pediente
-        </option>
-        <option
-            value="1"
-            @if(isset($values))
-                @foreach($values as $value)
-                    @if($value->boolDeposited == 1)
-                    selected
-                    @endif
-                @endforeach
+        <option value="0" @if(isset($values)) @foreach($values as $value) @if($value->boolDeposited == 0)
+            selected
             @endif
-        >
-        Depósito Realizado
+            @endforeach
+            @endif
+            >
+            Pediente
+        </option>
+        <option value="1" @if(isset($values)) @foreach($values as $value) @if($value->boolDeposited == 1)
+            selected
+            @endif
+            @endforeach
+            @endif
+            >
+            Depósito Realizado
         </option>
     </select>
 </div>
 
 <div class="input-group mb-3">
     <label class="input-group-text" for="depositDateInput">Fecha de Depósito</label>
-    <input
-        type="date"
-        class="date form-control"
-        name="depositDateInput"
-        @if(isset($values))
-            @foreach($values as $value)
-                value={{ $value->datDepositedDate }}
-            @endforeach
-        @endif
-        id="depositDateInput" autocomplete="off"
-    >
+    <input type="date" class="date form-control" name="depositDateInput" @if(isset($values)) @foreach($values as $value) value={{ $value->datDepositedDate }} @endforeach @endif id="depositDateInput" autocomplete="off">
 </div>
 
 <br>

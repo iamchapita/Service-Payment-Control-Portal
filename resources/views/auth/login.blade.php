cfe14716f4a0ea265b79b5ed6c9074100f3bb01d
iamchapita <alejandrom646@gmail.com>
2022-11-25 11:52:46 -0600

    Creacion de formulario de inicio de sesion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,19 @@
 @include('headers')
 @include('navbar')
-<form class="loginRegistrationForm">
+<form class="loginRegistrationForm" method="post" action="{{ route('loginStep') }}">
+    @csrf
     <!-- Email input -->
     <div class="form-outline mb-4">
-        <input type="text" class="form-control" required autocomplete="off" id="email" name="email"/>
-        <label class="form-label" for="form2Example1">Nombre de Usuario</label>
+        <input type="text" class="form-control" required autocomplete="off" id="name" name="name" placeholder="Un solo nombre" pattern="[A-Za-z]{4,20}" title="Letras y números. Tamaño mínimo: 4. Tamaño máximo: 20"/>
+        <label class="form-label" for="name">Nombre</label>
     </div>
 
     <!-- Password input -->
     <div class="form-outline mb-4">
-        <input type="password" class="form-control" required autocomplete="off" id="password" name="password"/>
+        <input type="password" class="form-control" required autocomplete="off" id="password" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,35}$" placeholder="Contraseña de 8 a 35 caractéres"/>
         <label class="form-label" for="form2Example2">Contraseña</label>
     </div>
 
     <!-- Submit button -->
-    <button type="button" class="btn btn-primary btn-block mb-4">Iniciar Sesión</button>
+    <button type="submit" class="btn btn-primary btn-block mb-4">Iniciar Sesión</button>
 </form>

0079784a6c2e09bda4a96421e7ca28f27d495985
iamchapita <alejandrom646@gmail.com>
2022-11-28 16:58:15 -0600

    Cambio de nombre de rutas

--- routes/web.php
+++ routes/web.php
@@ -25,25 +25,25 @@ Route::resource('User', UserController::class);
 // Rutas individuales
 // The function or view end with Step is used to make backend operations
 // to make possible the action (login or logout)
-Route::get('/', [UserController::class, 'usersList'])->name('home');
-Route::get('/login', [AuthController::class, 'loginView'])->name('login');
-Route::post('/loginStep', [AuthController::class, 'loginStep'])->name('loginStep');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/passwordChange', [AuthController::class, 'passwordChangeView'])->name('passwordChange');
-Route::post('/passwordChangeStep', [AuthController::class, 'passwordChangeStep'])->name('passwordChangeStep');
+Route::get('/', [UserController::class, 'usersList'])->name('Home');
+Route::get('/login', [AuthController::class, 'loginView'])->name('Login');
+Route::post('/loginStep', [AuthController::class, 'loginStep'])->name('LoginStep');
+Route::get('/logout', [AuthController::class, 'logout'])->name('Logout');
+Route::get('/passwordChange', [AuthController::class, 'passwordChangeView'])->name('PasswordChange');
+Route::post('/passwordChangeStep', [AuthController::class, 'passwordChangeStep'])->name('PasswordChangeStep');
 
 
 // Rutas Personalizadas
 Route::prefix('/PaymentDetail')->group(function () {
-    Route::get('/', [PaymentDetailController::class, 'index'])->name('historicalDetail');
-    Route::get('/SpotifyDetail', [PaymentDetailController::class, 'spotifyDetail'])->name('spotifyDetail');
-    Route::get('/NetflixyDetail', [PaymentDetailController::class, 'netflixDetail'])->name('netflixDetail');
-    Route::get('/DisneyDetail', [PaymentDetailController::class, 'disneyDetail'])->name('disneyDetail');
-    Route::get('/create', [PaymentDetailController::class, 'create'])->name('createPaymentDetail');
+    Route::get('/', [PaymentDetailController::class, 'index'])->name('PaymentDetail');
+    Route::get('/SpotifyDetail', [PaymentDetailController::class, 'spotifyDetail'])->name('SpotifyDetail');
+    Route::get('/NetflixyDetail', [PaymentDetailController::class, 'netflixDetail'])->name('NetflixDetail');
+    Route::get('/DisneyDetail', [PaymentDetailController::class, 'disneyDetail'])->name('DisneyDetail');
+    Route::get('/create', [PaymentDetailController::class, 'create'])->name('CreatePaymentDetail');
     // Contiene los detalles de pago de servicio personal
-    Route::post('/UserDetail', [PaymentDetailController::class, 'userDetail'])->name('userDetail');
-    Route::post('/insert', [PaymentDetailController::class, 'store'])->name('insertPaymentDetail');
-    Route::post('/{id}/edit', [PaymentDetailController::class, 'edit'])->name('editPaymentDetail');
-    Route::put('/{id}/update', [PaymentDetailController::class, 'update'])->name('updatePaymentDetail');
-    Route::delete('/{id}/delete', [PaymentDetailController::class, 'destroy'])->name('destroyPaymentDetail');
+    Route::post('/UserDetail', [PaymentDetailController::class, 'userDetail'])->name('UserDetail');
+    Route::post('/insert', [PaymentDetailController::class, 'store'])->name('InsertPaymentDetail');
+    Route::post('/{id}/edit', [PaymentDetailController::class, 'edit'])->name('EditPaymentDetail');
+    Route::put('/{id}/update', [PaymentDetailController::class, 'update'])->name('UpdatePaymentDetail');
+    Route::delete('/{id}/delete', [PaymentDetailController::class, 'destroy'])->name('DestroyPaymentDetail');
 });

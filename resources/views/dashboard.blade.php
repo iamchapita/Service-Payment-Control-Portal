7def89589160f2b6e2cc1d6b3d5ddcb5eb6682db
iamchapita <alejandrom646@gmail.com>
2022-11-28 22:43:02 -0600

    Implementeacion de dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,21 +2,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
-                <div class="card-body">
-                    @if(session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{session('status')}}
+        @isset($views)
+        @foreach($views as $view)
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $view }}</h5>
+                        <p class="card-text">Administre la informacion de {{ $view }} almacenada.</p>
+                        <a href="{{ route( $view ) }}" class="btn btn-dark">{{ $view }}</a>
                     </div>
-                    @endif
-                    You are logged in!
                 </div>
             </div>
-        </div>
+        @endforeach
+        @endisset
     </div>
+</div>
 </div>
 @endsection

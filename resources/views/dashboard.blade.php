aadf80cc94bc16cd21ec08dfeff7d95e1fcedce0
iamchapita <alejandrom646@gmail.com>
2022-11-28 22:55:37 -0600

    Implementacion de condicion para evitar que se muestre dashboard como tarjeta

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,17 @@
     <div class="row justify-content-center">
         @isset($views)
         @foreach($views as $view)
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $view }}</h5>
-                        <p class="card-text">Administre la informacion de {{ $view }} almacenada.</p>
-                        <a href="{{ route( $view ) }}" class="btn btn-dark">{{ $view }}</a>
+            @if($view != 'Dashboard')
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $view }}</h5>
+                            <p class="card-text">Administre la informacion de {{ $view }} almacenada.</p>
+                            <a href="{{ route( $view ) }}" class="btn btn-dark">{{ $view }}</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @endforeach
         @endisset
     </div>

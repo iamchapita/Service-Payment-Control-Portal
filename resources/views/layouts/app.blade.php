5c28b4ff0aeba1929d1d2356375f05887aa8aca6
iamchapita <alejandrom646@gmail.com>
2022-12-01 02:46:49 -0600

    Instalacion de bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
-        <!-- Bootstrap Bundle with Popper -->
-        <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <script src="{{asset('js/spp.js')}}" defer></script>
+
         <script src="{{asset('js/filter.js')}}"></script>
         <script src="{{asset('js/clearSelect.js')}}"></script>
         <script src="{{asset('js/checkField.js')}}"></script>

666ee1249e38a2f5922870f22725ed3a8ec5a1ad
iamchapita <alejandrom646@gmail.com>
2022-12-02 21:12:24 -0600

    Cambios Varios: - Implementacion de funcion de validacion. - Implementacion de operacion de insercion.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,40 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+
+    protected function validator(Request $request){
+
+        // Estableciendo los nombres personalizados de los atributos
+        $attributes = [
+            'texName' => 'Nombre',
+            'boolStatus' => 'Estado del Usuario',
+            'boolAdminStatus' => 'Rol del Usuario',
+        ];
+
+        // Personalizando los mensajes de error
+        $messages = [
+            'texName.required' => 'El atributo Nombre es requerido.',
+            'texName.min' => 'El atributo Nombre debe tener como mínimo 4.',
+            'texName.max' => 'El atributo Nombre debe tener como máximo 8.',
+            'boolStatus.required' => 'El atributo Estado es requerido.',
+            'boolAdminStatus.required' => 'El atributo Rol es requerido.'
+        ];
+
+        // Estableciendo las reglas de validacion
+        $rules = [
+            'texName' => 'required|string|min:4|max:20',
+            'boolStatus' => 'required|numeric|in:0,1',
+            'boolAdminStatus' => 'required|numeric|in:0,1',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages, $attributes);
+
+        return $validator;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -16,26 +47,73 @@ class UserController extends Controller
      */
     public function index()
     {
+        // Variables para la vista
         $data['values'] = User::all(['id', 'texName', 'boolStatus', 'boolAdminStatus']);
         $data['currentView'] = 'User';
         $data['views'] = array('Dashboard', 'PaymentDetail', 'User', 'Service');
         $data['elementsDropdown'] = array('Histórico Spotify', 'Histórico Netflix', 'Histórico Disney+');
         $data['elementsDropdownLinks'] = array('SpotifyDetail', 'NetflixDetail', 'DisneyDetail');
+        // URL del formulario
+        $data['insertURL'] = 'CreateUser';
 
         return view('User.index', $data);
     }
 
+    // Obtiene todos los usuarios activos y con rol de cliente y los retorna a la vista
     public function usersList()
     {
         $data['values'] = User::where([['boolStatus', '=', '1'], ['boolAdminStatus', '=', '0']])->get();
         return view('FirstScreen.index', $data);
     }
 
-    public static function updatePassword($password)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $user = User::where('boolAdminStatus', '1')->first();
-        // $user->password = Hash::make($password);
-        $user->password = $password;
-        $user->save();
+        // Variables para la vista
+        $data['currentView'] = 'Create User';
+        $data['views'] = array('Dashboard', 'PaymentDetail', 'User', 'Service');
+        $data['elementsDropdown'] = array('Historico Spotify', 'Historico Netflix', 'Historico Disney+');
+        $data['elementsDropdownLinks'] = array('SpotifyDetail', 'NetflixDetail', 'DisneyDetail');
+        // URL del formulario
+        $data['formURL'] = 'InsertUser';
+        $data['title'] = 'Insertar Nuevo Registro en User';
+        // Texto del boton del formulario
+        $data['action'] = 'Insertar';
+
+        return view('User.headerForm', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validando los campos
+        $validator = $this->validator($request);
+
+        // Verificando si la validacion es correcta o no
+        if($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+
+        }else{
+
+            // Instancia de User para la posterior insecion
+            $user = new User();
+            $user->texName = $request->input('texName');
+            $user->boolStatus = $request->input('boolStatus');
+            $user->boolAdminStatus = $request->input('boolAdminStatus');
+
+            $user->save();
+
+            return redirect(route('User'));
+        }
+
     }
 }

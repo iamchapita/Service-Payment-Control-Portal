a41cf19a4f7bc8e43442651860f7c238afaa39e7
iamchapita <alejandrom646@gmail.com>
2023-01-04 11:52:08 -0600

    Cambio en configuracion para utilizacion de docker

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the Docker Container port mapping to work
+        strictPort: true,
+        port: 5173, // you can replace this port with any port
+    },
 });

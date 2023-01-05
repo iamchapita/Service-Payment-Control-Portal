27f44608f6f70ccf7b42e42d0726f742ee045e49
iamchapita <alejandrom646@gmail.com>
2023-01-05 00:14:06 -0600

    Implementacion de estilos para los filtros y botones

--- resources/views/filterAndButton.blade.php
+++ resources/views/filterAndButton.blade.php
@@ -1,18 +1,25 @@
 <h1>{{ $currentView }}</h1>
 <br>
 <div id="filterAndButton">
-    <div class="input-group mb-3">
-        <input type="text" class="form-control" id="searchInput" autocomplete="off" onkeyup="searchFilter()" placeholder="Filtrar" aria-label="Search" aria-describedby="basic-addon1">
-        @isset($years)
-        <select name="yearFilter" class="form-select" id="yearFilterInput" autocomplete="off" onchange="yearFilter()">
-            <option selected value="">Seleccione un Año</option>
-            @foreach( $years as $year )
-                <option value="{{ $year->year }}">{{ $year->year }}</option>
-            @endforeach
-        </select>
-        @endisset
+    <div class="row gy-2 gx-6">
+        <div class="col-8">
+            <div class="input-group">
+                <input type="text" class="form-control" id="searchInput" autocomplete="off" placeholder="Filtrar" aria-label="Search" aria-describedby="basic-addon1">
+                @isset($years)
+                <select name="yearFilterInput" class="form-select" id="yearFilterInput" autocomplete="off">
+                    <option selected value="">Seleccione un Año</option>
+                    @foreach( $years as $year )
+                    <option value="{{ $year->year }}">{{ $year->year }}</option>
+                    @endforeach
+                </select>
+                @endisset
+                <button id="filterButton" type="button" class="btn btn-primary" onclick="searchFilter()">Filtrar</button>
+            </div>
+        </div>
+        <div class="col-4">
+            @isset($insertURL)
+            <button id="insertButton" type="button" class="btn btn-secondary" onclick="location.href='{{ route($insertURL) }}'">Insertar</button>
+            @endisset
+        </div>
     </div>
-    @isset($insertURL)
-    <button id="insertButton" type="button" class="btn btn-secondary" onclick="location.href='{{ route($insertURL) }}'">Insertar</button>
-    @endisset
 </div>

3210875540cf6889c05d4f639569cbb3f6e9a77a
iamchapita <alejandrom646@gmail.com>
2023-01-04 20:17:25 -0600

    Agregado campo para filtrar el año

--- resources/views/filterAndButton.blade.php
+++ resources/views/filterAndButton.blade.php
@@ -3,6 +3,14 @@
 <div id="filterAndButton">
     <div class="input-group mb-3">
         <input type="text" class="form-control" id="searchInput" autocomplete="off" onkeyup="searchFilter()" placeholder="Filtrar" aria-label="Search" aria-describedby="basic-addon1">
+        @isset($years)
+        <select name="yearFilter" class="form-select" id="yearFilterInput" autocomplete="off" onchange="yearFilter()">
+            <option selected value="">Seleccione un Año</option>
+            @foreach( $years as $year )
+                <option value="{{ $year->year }}">{{ $year->year }}</option>
+            @endforeach
+        </select>
+        @endisset
     </div>
     @isset($insertURL)
     <button id="insertButton" type="button" class="btn btn-secondary" onclick="location.href='{{ route($insertURL) }}'">Insertar</button>

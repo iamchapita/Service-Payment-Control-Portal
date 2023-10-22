f264ec54772b644d98ef5b1444558cdde6faf7c1
iamchapita <alejandrom646@gmail.com>
2023-10-22 11:13:03 -0600

    Formateo

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,49 +1,48 @@
 @extends('layouts.app')
 @section('content')
-<div class='container'>
-    @include('filterAndButton')
-    <div class="table-responsive">
-        <table class="table table-bordered table-ligth table-hover  align-middle" id="table">
-            <thead class="table-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Nombre</th>
-                    <th>Estado</th>
-                    <th>Administrador</th>
-                    <th>Editar</th>
-                </tr>
-            </thead>
+    <div class='container'>
+        @include('filterAndButton')
+        <div class="table-responsive">
+            <table class="table table-bordered table-ligth table-hover  align-middle" id="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Estado</th>
+                        <th>Administrador</th>
+                        <th>Editar</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                @foreach ( $values as $value )
-                <tr>
-                    <td>{{ $value->id }}</td>
-                    <td>{{ $value->texName }}</td>
-                    <td>
-                        @if ( $value->boolStatus == 0 )
-                        Inactivo
-                        @else
-                        Activo
-                        @endif
-                    </td>
-                    <td>
-                        @if ( $value->boolAdminStatus == 0 )
-                        No
-                        @else
-                        Sí
-                        @endif
-                    </td>
-                    <td>
-                        <form method="POST" action="{{ route('EditUser', $value->id) }}">
-                            @csrf
-                            <button type="submit" class="btn btn-success">Editar</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                <tbody>
+                    @foreach ($values as $value)
+                        <tr>
+                            <td>{{ $value->id }}</td>
+                            <td>{{ $value->texName }}</td>
+                            <td>
+                                @if ($value->boolStatus == 0)
+                                    Inactivo
+                                @else
+                                    Activo
+                                @endif
+                            </td>
+                            <td>
+                                @if ($value->boolAdminStatus == 0)
+                                    No
+                                @else
+                                    Sí
+                                @endif
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route('EditUser', $value->id) }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success">Editar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
-
 @endsection

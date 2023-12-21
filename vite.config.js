a680e98b1a559853e4da46842e7e2859a1670bfc
iamchapita <alejandrom646@gmail.com>
2023-12-21 00:07:37 -0600

    Cambio en configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {
-        watch: {
-            usePolling: true,
-        },
-        host: true, // needed for the Docker Container port mapping to work
-        strictPort: true,
-        port: 5173, // you can replace this port with any port
-    },
 });

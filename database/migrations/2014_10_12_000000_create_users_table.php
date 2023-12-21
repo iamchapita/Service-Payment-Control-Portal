d6c2da9674de0208120306e0afbfd717b2634131
iamchapita <alejandrom646@gmail.com>
2023-12-21 01:16:45 -0600

    Cambios en user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -23,6 +24,15 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
             $table->rememberToken()->nullable();
         });
+
+        $user = new User();
+        $user->texName = 'Alejandro';
+        $user->boolStatus = true;
+        $user->boolAdminStatus = true;
+        $user->password = "IngeAlejandro98";
+
+        $user->save();
+
     }
 
     /**

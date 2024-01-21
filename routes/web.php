04aa33a9583dbd928e36badc4f0cb1e4efd243a3
iamchapita <alejandrom646@gmail.com>
2024-01-21 12:04:53 -0600

    Implementación de rutas para spotify2

--- routes/web.php
+++ routes/web.php
@@ -43,13 +43,14 @@ Route::prefix('/User')->group(function () {
 
 Route::prefix('/PaymentDetail')->group(function () {
     Route::get('/', [PaymentDetailController::class, 'index'])->middleware('auth')->name('PaymentDetail');
+    Route::get('/Spotify2Detail', [PaymentDetailController::class, 'spotify2Detail'])->middleware('auth')->name('Spotify2Detail');
     Route::get('/SpotifyDetail', [PaymentDetailController::class, 'spotifyDetail'])->middleware('auth')->name('SpotifyDetail');
     Route::get('/NetflixyDetail', [PaymentDetailController::class, 'netflixDetail'])->middleware('auth')->name('NetflixDetail');
     Route::get('/DisneyDetail', [PaymentDetailController::class, 'disneyDetail'])->middleware('auth')->name('DisneyDetail');
     Route::match(['get', 'post'], '/Create', [PaymentDetailController::class, 'create'])->middleware('auth')->name('CreatePaymentDetail');
     Route::match(['get', 'post'], '/UserDetail', [PaymentDetailController::class, 'userDetail'])->name('UserDetail');
     Route::post('/Insert', [PaymentDetailController::class, 'store'])->middleware('auth')->name('InsertPaymentDetail');
-    Route::match(['get', 'post'],'/{id}/edit', [PaymentDetailController::class, 'edit'])->middleware('auth')->name('EditPaymentDetail');
+    Route::match(['get', 'post'], '/{id}/edit', [PaymentDetailController::class, 'edit'])->middleware('auth')->name('EditPaymentDetail');
     Route::put('/{id}/update', [PaymentDetailController::class, 'update'])->middleware('auth')->name('UpdatePaymentDetail');
     Route::delete('/{id}/delete', [PaymentDetailController::class, 'destroy'])->middleware('auth')->name('DestroyPaymentDetail');
 });
